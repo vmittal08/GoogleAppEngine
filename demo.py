ad8a07597199c79cce1db27b022a527fdb5d5646
vmittal08 <mittalvidhi08@gmail.com>
2025-02-11 09:16:03 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,7 @@
+#VIDHI MITTAL (roll no: 40)
+number = int(input("Enter a number to check for odd or even: "))
+
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
